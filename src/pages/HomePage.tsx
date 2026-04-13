c4760247332984e80108499357429734735a9720
renesurhuizum <renedeboer97@gmail.com>
2026-04-13 15:16:57 +0200

    fix: correct section order in HomePage and remove legacy imports

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Hero from '../components/sections/Hero';
 import SocialProofBar from '../components/sections/SocialProofBar';
 import PainSection from '../components/sections/PainSection';
@@ -8,11 +7,6 @@ import Services from '../components/sections/Services';
 import TestimonialsSection from '../components/sections/TestimonialsSection';
 import CTASection from '../components/sections/CTASection';
 import FAQ from '../components/sections/FAQ';
-import { PartnersSection } from '../components/sections/PartnersSection';
-import { AIBenefits } from '../components/sections/AIBenefits';
-import { USPs } from '../components/sections/USPs';
-import { ROICalculator } from '../components/sections/ROICalculator';
-import { SubsidieSection } from '../components/sections/SubsidieSection';
 
 export const HomePage = () => {
   return (
@@ -24,13 +18,8 @@ export const HomePage = () => {
       <UseCasesPreview />
       <Services />
       <TestimonialsSection />
-      <AIBenefits />
-      <USPs />
-      <ROICalculator />
-      <SubsidieSection />
-      <PartnersSection />
-      <FAQ />
       <CTASection />
+      <FAQ />
     </>
   );
 };
